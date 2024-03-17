5b984d946de40c22b3ca5ded617b7e81509834ba
Farhan <animationfar@gmail.com>
2024-03-17 16:06:20 +0700

    feat: setup next-auth and insertion

--- prisma/insert.ts
+++ prisma/insert.ts
@@ -1,1 +1,22 @@
-//
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+import { db } from "~/server/db";
+import bcrypt from "bcrypt";
+
+const hash = async (password: string) => bcrypt.hash(password, await bcrypt.genSalt(10));
+
+async function upsert() {
+    await db.user.createMany({
+        data: [
+            {
+                id: 1,
+                name: "Administrator",
+                username: "Admin",
+                password: await hash("12345"),
+                level: "Admin",
+            },
+        ],
+    });
+}
+
+void upsert();
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,10 +6,13 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
+import { type User as $User, type UserLevel } from "@prisma/client";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -20,17 +23,23 @@ import { db } from "~/server/db";
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
-      id: string;
+      id: $User["id"];
+      name: $User["name"];
+      username: $User["username"];
+      level: UserLevel;
       // ...other properties
       // role: UserRole;
     };
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+  interface User {
+    id: $User["id"];
+    level: UserLevel;
+    username: $User["username"];
+  }
 }
+
+const adapter = PrismaAdapter(db) as Adapter;
 
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
@@ -44,24 +53,56 @@ export const authOptions: NextAuthOptions = {
       user: {
         ...session.user,
         id: user.id,
+        level: user.level,
+        username: user.username,
+        name: user.name,
       },
     }),
   },
-  adapter: PrismaAdapter(db) as Adapter,
+  jwt: {
+    async encode({ secret, token, maxAge }) {
+      const newToken = jwt.sign(token as object, secret, { 
+        algorithm: "HS256",
+        expiresIn: maxAge,
+      });
+
+      let session;
+      if (token != null && typeof token.sub == "string" && adapter.createSession != undefined) {
+        session = await adapter.createSession({
+          userId: token.sub,
+          sessionToken: newToken,
+          expires: new Date(new Date().getTime() + ((maxAge ?? 0) * 1000)),
+        });
+      }
+
+      return newToken;
+    },
+  },
+  adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username", type: "text", },
+        password: { label: "Password", type: "password", },
+      },
+      async authorize(credentials, req) {
+        const user = await db.user.findFirst({
+          where: {
+            username: credentials!.username,
+          },
+        });
+
+        if (!user) return null;
+        if (! await bcrypt.compare(credentials!.password, user.password) ) return null;
+
+        return {
+          id: user.id,
+          level: user.level,
+          username: user.username,
+          name: user.name,
+        };
+      },
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
