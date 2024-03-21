e0eef341b09b1f1b004b47909906ac1b2e882fb4
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-03-21 20:54:39 +0000

    test: proof upload files works

--- src/pages/test/01_test_upload_costum.tsx
+++ src/pages/test/01_test_upload_costum.tsx
@@ -1,35 +1,70 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useForm } from 'react-hook-form';
+import { api } from "~/utils/api";
 
 export default function TestUploadCostum() {
+    const { register, watch, setValue, getValues } = useForm<{
+        name: string;
+        image: string;
+        link: string;
+        origin: string;
+        preference: string;
+        kset: string;
+    }>();
+    const [file, setFile] = useState<File>();
+
+    const { mutateAsync: addKostum, status, ...resp } = api.kostum.addKostum.useMutation();
+
+    useEffect(() => {
+        console.log(file);
+        const x = watch((val, {name, type: xtype}) => {
+            console.log(val, name, xtype);
+        });
+        console.log(status, resp);
+        () => x.unsubscribe();
+    }, [file, watch, status]);
+
+    const uploadFile = async () => {
+        if (!file) alert("No Files");
+        const data = new FormData();
+        data.append("file", file!);
+        try {
+            const fe = await fetch("/api/upload", {
+                method: "POST",
+                body: data,
+            });
+            const json = await fe.json();
+            const name = json.name as string;
+            console.log(name, json);
+            setValue("image", name, { shouldDirty: true, shouldTouch: true });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return <>
         <Box sx={{ px: 5, py: 2, display: 'flex', flexDirection: 'column', gap: 5 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: { mobile: 'column-reverse', laptop: 'row' } }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', width: { mobile: '100%', laptop: '70%' } }}>
-                    <Typography variant='h5' sx={{ fontWeight: '600', textAlign: { mobile: 'center', laptop: 'start' } }}>What is Conscision?</Typography>
-                    <Typography sx={{ textAlign: { mobile: 'center', laptop: 'start' } }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis consequuntur ducimus aut ullam minima iure doloremque quo vero nihil ad cum voluptas itaque quam animi atque, voluptates, dolores rem! Tenetur error magnam fugit sequi nobis eaque veritatis, illo, non aliquid aspernatur facere impedit? Ad quae, aliquam placeat voluptatum consequatur asperiores expedita odit fugit voluptatem numquam, corrupti recusandae architecto, aut veritatis aperiam dolorem sequi id iure beatae. Magnam reprehenderit est deserunt asperiores maxime neque, accusamus, enim ex nemo esse aut.</Typography>
-                </Box>
-                <Box sx={{ width: { mobile: '60%', laptop: '15%' }, margin: 'auto' }}>
-                    <Image src={'/assets/coscision-logo.png'} alt='logo' width={2000} height={2000} style={{ width: '100%', height: '100%', margin: 'auto' }} />
-                </Box>
+            <Box sx={{ mt: 10, }}>
+                <input type="file" onChange={(ev) => setFile(ev.target?.files?.[0])} />
+                <Button variant="contained" onClick={uploadFile}>Send</Button>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: { mobile: 'column', laptop: 'row' } }}>
-                <Box sx={{ width: { mobile: '60%', laptop: '15%' }, margin: 'auto' }}>
-                    <Image src={'/assets/about2.jpg'} alt='logo' width={2000} height={2000} style={{ width: '100%', height: '100%', margin: 'auto', borderRadius: 50 }} />
-                </Box>
-                <Box sx={{ display: 'flex', flexDirection: 'column', width: {mobile: '100%', laptop: '70%'} }}>
-                    <Typography variant='h5' sx={{ fontWeight: '600', textAlign: { mobile: 'center', laptop: 'right' } }}>What will you get?</Typography>
-                    <Typography sx={{ textAlign: { mobile: 'center', laptop: 'right' } }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis consequuntur ducimus aut ullam minima iure doloremque quo vero nihil ad cum voluptas itaque quam animi atque, voluptates, dolores rem! Tenetur error magnam fugit sequi nobis eaque veritatis, illo, non aliquid aspernatur facere impedit? Ad quae, aliquam placeat voluptatum consequatur asperiores expedita odit fugit voluptatem numquam, corrupti recusandae architecto, aut veritatis aperiam dolorem sequi id iure beatae. Magnam reprehenderit est deserunt asperiores maxime neque, accusamus, enim ex nemo esse aut.</Typography>
-                </Box>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: { mobile: 'column-reverse', laptop: 'row' } }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', width: { mobile: '100%', laptop: '70%' } }}>
-                    <Typography variant='h5' sx={{ fontWeight: '600', textAlign: { mobile: 'center', laptop: 'start' } }}>About SAW Method</Typography>
-                    <Typography sx={{ textAlign: { mobile: 'center', laptop: 'start' } }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. At blanditiis consequuntur ducimus aut ullam minima iure doloremque quo vero nihil ad cum voluptas itaque quam animi atque, voluptates, dolores rem! Tenetur error magnam fugit sequi nobis eaque veritatis, illo, non aliquid aspernatur facere impedit? Ad quae, aliquam placeat voluptatum consequatur asperiores expedita odit fugit voluptatem numquam, corrupti recusandae architecto, aut veritatis aperiam dolorem sequi id iure beatae. Magnam reprehenderit est deserunt asperiores maxime neque, accusamus, enim ex nemo esse aut.</Typography>
-                </Box>
-                <Box sx={{ width: { mobile: '60%', laptop: '15%' }, margin: 'auto' }}>
-                    <Image src={'/assets/about3.png'} alt='logo' width={2000} height={2000} style={{ width: '100%', height: '100%', margin: 'auto', borderRadius: '50%' }} />
-                </Box>
+            <Box sx={{ mt: 10, }}>
+                <TextField id="outlined-basic" label="name" variant="outlined" { ...register("name") } />
+                <TextField id="outlined-basic" label="image" variant="outlined" { ...register("image") } />
+                <TextField id="outlined-basic" label="link" variant="outlined" { ...register("link") } />
+                <TextField id="outlined-basic" label="origin" variant="outlined" { ...register("origin") } />
+                <TextField id="outlined-basic" label="preference" variant="outlined" { ...register("preference") } />
+                <TextField id="outlined-basic" label="kset" variant="outlined" { ...register("kset") } />
+                <Button variant="contained" onClick={() => void addKostum({
+                    image: getValues("image"),
+                    kset: getValues("kset"),
+                    link: getValues("link"),
+                    name: getValues("name"),
+                    origin: getValues("origin"),
+                    preference: getValues("preference") as ("Game" | "Anime" | "Vtuber"),
+                })}>Send</Button>
             </Box>
         </Box>
     </>;
